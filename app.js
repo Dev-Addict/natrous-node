ee697412117fc311b99e5251337ace5722ddefb4
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-06 22:28:02 +0430

    using morgan third party middleware for express

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
