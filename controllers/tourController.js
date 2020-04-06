0e0a0cd8234601746ef615cc67bbfe9c2c530ee1
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-07 03:00:40 +0430

    working with dot(.)env and config

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,6 +14,18 @@ exports.checkId = (req, res, next, val) => {
   }
   next();
 };
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      requestTime: req.requestTime,
+      message: 'the request body doesn\'t have required params(title or price)'
+    });
+  }
+  next();
+};
+
 exports.getTours = (req, res) => {
   res.status(200).json({
     status: 'success',
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+const port = process.env.PORT || 3000;
+
 const app = require('./app');
 
-const port = 3000;
+dotenv.config({path:'./config.env'});
+
 app.listen(port, () => {
   console.log(`App is running in port${port}`);
 });
