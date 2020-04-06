3fa0b59cc1c2077c43ed9f1891751b2d6fd2ede8
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-06 17:36:32 +0430

    get single tour using our api

--- app.js
+++ app.js
@@ -8,30 +8,48 @@ app.use(express.json());
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find(el => el.id === req.params.id * 1);
+
+  if (tour) {
     res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {tours}
+      status: 'success',
+      data: {
+        tour
+      }
     });
+  } else {
+      res.status(404).json({
+          status: 'fail',
+          message: 'Invalid ID'
+      });
+  }
 });
 
 app.post('/api/v1/tours', (req, res) => {
-    const id = tours[tours.length - 1];
-    const tour = Object.assign({id}, req.body);
+  const id = tours[tours.length - 1];
+  const tour = Object.assign({ id }, req.body);
 
-    tours.push(tour);
+  tours.push(tour);
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour
-            }
-        });
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour
+      }
     });
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App is running in port${port}`);
+  console.log(`App is running in port${port}`);
 });
