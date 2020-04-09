5b587c82574c392050872b6ff972188578434258
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-09 16:56:31 +0430

    create Tour Model

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const dotenv = require('dotenv');
-const port = process.env.PORT || 3000;
 
 const app = require('./app');
 
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
+const DB =
+  process.env.DATABASE
+    .replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running in port${port}`);
 });
