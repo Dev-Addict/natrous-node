ed8c0e2a39d46d3531a4e9d379dddf875790ef35
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-09 23:40:06 +0430

    updated tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,17 +3,54 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A Tour Must Have Name'],
-    unique: true
+    required: [true, 'A Tour Must Have a name'],
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
     type: Number,
-    default: 4.5
+    required: [true, 'A Tour Must Have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour Must Have a maxGroupSize']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A Tour Must Have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
-    required: [true, 'A Tour Must Have Price']
-  }
+    required: [true, 'A Tour Must Have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A Tour Must Have a description']
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour Must Have a imageCover']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
