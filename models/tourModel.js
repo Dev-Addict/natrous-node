1827b3fbc8bb3a766e7f596830fe026eac502332
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-11 16:16:34 +0430

    write some validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,9 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A Tour Must Have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxLength: [40, 'A Tour Must Have a name With Less Than 40 Characters'],
+    minLength: [10, 'A Tour Must Have a name With At List 10 Characters']
   },
   duration: {
     type: Number,
@@ -17,11 +19,18 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A Tour Must Have a difficulty']
+    required: [true, 'A Tour Must Have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message:
+        'A Tour Must Have difficulty Value Set To easy Or medium Or difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 0
+    default: 0,
+    min: [1, 'A Tour Must Have ratingsAverage Equal Or Greater Than 1.0'],
+    max: [5, 'A Tour Must Have ratingsAverage Equal Or Smaller Than 5.0']
   },
   ratingsQuantity: {
     type: Number,
@@ -31,7 +40,15 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A Tour Must Have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(value) {
+        return val < this.price && val > 0;
+      },
+      message: 'A Tour Must Have price Equal Or Greater Than 0 Or Smaller Than price'
+    }
+  },
   summary: {
     type: String,
     trim: true
@@ -56,8 +73,8 @@ const tourSchema = new mongoose.Schema({
     default: false
   }
 }, {
-  toJSON: {virtuals: true},
-  toObject: {virtuals: true}
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 tourSchema.virtual('durationWeek').get(function() {
@@ -75,14 +92,14 @@ tourSchema.post('save', function(doc, next) {
 });
 
 tourSchema.pre(/^find/g, function(next) {
-  this.find({secretTour: {$ne: true}});
+  this.find({ secretTour: { $ne: true } });
   next();
 });
 
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({
     $match: {
-      $secretTour: {$ne: true}
+      $secretTour: { $ne: true }
     }
   });
   next();
