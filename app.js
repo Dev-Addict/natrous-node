c690ddad4f608c969b88fe3ced2c76eaceda9876
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-11 18:25:03 +0430

    create global error handling middleware(amazing name yeah)

--- app.js
+++ app.js
@@ -20,10 +20,21 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can\'t find ${req.originalUrl} on this server.`
+app.all('*', (req, res, next) => {
+  const error = new Error(`Can\'t find ${req.originalUrl} on this server.`);
+  error.statusCode = 404;
+  error.status = 'fail';
+
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
