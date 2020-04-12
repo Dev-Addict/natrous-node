9f8fbf9431a9cb355f64630a5707e6b1a9f94913
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-12 17:16:56 +0430

    update the way of handle unhandled rejection

--- server.js
+++ server.js
@@ -19,10 +19,13 @@ mongoose.connect(DB, {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running in port${port}`);
 });
 
 process.on('unhandledRejection', err => {
   console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
