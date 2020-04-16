159fd84d76bbb2428ce2c8987e46e72eb9969854
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-16 16:14:46 +0430

    update tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,81 +1,106 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A Tour Must Have a name'],
-    unique: true,
-    trim: true,
-    maxLength: [40, 'A Tour Must Have a name With Less Than 40 Characters'],
-    minLength: [10, 'A Tour Must Have a name With At List 10 Characters']
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A Tour Must Have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A Tour Must Have a maxGroupSize']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A Tour Must Have a difficulty'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message:
-        'A Tour Must Have difficulty Value Set To easy Or medium Or difficult'
-    }
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-    min: [1, 'A Tour Must Have ratingsAverage Equal Or Greater Than 1.0'],
-    max: [5, 'A Tour Must Have ratingsAverage Equal Or Smaller Than 5.0']
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A Tour Must Have a price']
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function(value) {
-        return val < this.price && val > 0;
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A Tour Must Have a name'],
+      unique: true,
+      trim: true,
+      maxLength: [40, 'A Tour Must Have a name With Less Than 40 Characters'],
+      minLength: [10, 'A Tour Must Have a name With At List 10 Characters']
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A Tour Must Have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A Tour Must Have a maxGroupSize']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A Tour Must Have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message:
+          'A Tour Must Have difficulty Value Set To easy Or medium Or difficult'
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+      min: [1, 'A Tour Must Have ratingsAverage Equal Or Greater Than 1.0'],
+      max: [5, 'A Tour Must Have ratingsAverage Equal Or Smaller Than 5.0']
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A Tour Must Have a price']
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(value) {
+          return val < this.price && val > 0;
+        },
+        message: 'A Tour Must Have price Equal Or Greater Than 0 Or Smaller Than price'
+      }
+    },
+    summary: {
+      type: String,
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A Tour Must Have a description']
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A Tour Must Have a imageCover']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
       },
-      message: 'A Tour Must Have price Equal Or Greater Than 0 Or Smaller Than price'
-    }
-  },
-  summary: {
-    type: String,
-    trim: true
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A Tour Must Have a description']
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A Tour Must Have a imageCover']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: 'String',
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
+  }, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-}, {
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
-});
+);
 
 tourSchema.virtual('durationWeek').get(function() {
   return this.duration / 7;
