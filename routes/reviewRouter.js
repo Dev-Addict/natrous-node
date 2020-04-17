4cf7195b46db637742bce964bd4f94e42b14c4b8
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-17 19:48:04 +0430

    use tour router middleware for review router

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const reviewController = require('../controllers/reviewController');
 
-const router = express.Router();
+const router = express.Router(
+  {
+    mergeParams: true
+  }
+);
 
 router
   .route('/')
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -3,8 +3,11 @@ const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('../routes/reviewRouter');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -24,13 +27,5 @@ router
   .get(authController.protect, tourController.getTour)
   .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour)
   .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('users'),
-    reviewController.createTour
-  );
 
 module.exports = router;
