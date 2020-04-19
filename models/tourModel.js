169bdee9bd0cf12616617630039dbb0dae5b083c
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-19 20:26:20 +0430

    using index in mongoDB

--- models/tourModel.js
+++ models/tourModel.js
@@ -108,6 +108,11 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({
+  price: 1,
+  ratingsAverage: -1
+});
+
 tourSchema.virtual('durationWeek').get(function() {
   return this.duration / 7;
 });
