f9956dfc151dd3812706e59589950b901d48afa3
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-21 21:34:05 +0430

    build the base of project

--- app.js
+++ app.js
@@ -57,12 +57,16 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'WolfMAN'
+  });
 });
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/review', reviewRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', (req, res, next) => {
   next(
