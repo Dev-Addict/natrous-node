b11efc94a99762761031c321a7300ad921346447
WolfMAN <aria.azadi.pour@protonmail.com>
2020-04-25 11:15:06 +0430

    work with email a bit more

--- utils/email.js
+++ utils/email.js
@@ -1,21 +1,48 @@
 const nodeMailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.name = user.name;
+    this.url = url;
+    this.from = `natrous <${process.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 'placeHolder'
     }
-  });
-  const mailOptions = {
-    from: 'natrous <aria.azadi.pour@protonmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
-  await transporter.sendMail(mailOptions);
-};
+    return nodeMailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-module.exports = sendEmail;
+  async send(template, subject) {
+    const htmlTemplate = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      name: this.name,
+      url: this.url,
+      subject
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html: htmlTemplate,
+      text: htmlToText.fromString(htmlTemplate)
+    };
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to natrous Family!')
+  }
+};
